787d86b5ac918e480f5f1fa203d4f541dcd97c50
SigureRuri <50079507+SigureRuri@users.noreply.github.com>
2022-05-21 09:34:04 +0900

    initial commit

---
+++ src/main/java/com/github/sigureruri/ntublock/NtuBlock.kt
@@ -0,0 +1,13 @@
+package com.github.sigureruri.ntublock
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NtuBlock : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
